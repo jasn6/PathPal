fc1263430b55893f8567a39338ed4691f60abf27
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-11-10 16:14:35 -0500

    endpoint for getting nearby locations
    The nearbylocations endpoint makes a fetch request to the tripadvisor api to retrieve 10 nearby locations via specific provided lat and lng coordinates. This commit also includes checking if location details are already stored for a specific locationID in the getDetails endpoint.

--- api/routes/location.js
+++ api/routes/location.js
@@ -3,10 +3,37 @@ const Location = require("../models/Location.js");
 
 const LocationRouter = express.Router();
 
+LocationRouter.get("/nearbyLocations/:lat/:lng", async (req, res) => {
+  try {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    const response = await fetch(
+      `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${req.params.lat}%2C${req.params.lng}&language=en&key=${process.env.TRIPADV_KEY}`,
+      options
+    );
+    const data = await response.json();
+
+    // Extract location IDs
+    const locationIds = data.data.map((location) => location.location_id);
+
+    res.status(200).json({ locationIds });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch nearby locations" });
+  }
+});
+
 LocationRouter.get("/getDetails/:locationID", async (req, res) => {
   try {
     const options = { method: "GET", headers: { accept: "application/json" } };
     const locationID = req.params.locationID;
+
+    // Check if location already exists in the database
+    const existingLocation = await Location.findOne({ locationID });
+    if (existingLocation) {
+      return res.status(200).json(existingLocation);
+    }
+
     // Fetch Location Details
     const locationDetailsResponse = await fetch(
       `https://api.content.tripadvisor.com/api/v1/location/${locationID}/details?language=en&currency=USD&key=${process.env.TRIPADV_KEY}`,
