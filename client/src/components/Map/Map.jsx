704589d967f84075af9f859c181496be0ef022f8
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-11-16 23:40:36 -0500

    new map design

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,57 +1,54 @@
-import React, { useEffect } from "react";
+import React from "react";
 import GoogleMapReact from "google-map-react";
-import { Paper, Typography, Rating } from "@mui/material";
 import mapStyles from "./MapStyles";
 import "./styles.css";
 
-export default function Map({ coords, places }) {
+const Marker = ({ place, text, hovered, color }) => (
+  <div
+    className={`custom-marker ${hovered ? "hovered" : ""}`}
+    style={{
+      backgroundColor: color,
+    }}
+  >
+    <div className="inner-circle">{text}</div>
+    <div className="tooltip" style={hovered ? { display: "block" } : {}}>
+      {place.locationDetails.name}
+    </div>
+  </div>
+);
+
+export default function Map({ coords, locations }) {
   return (
     <div className="mapContainer">
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_KEY }}
         defaultCenter={coords}
-        margin={[50, 50, 50, 50]}
+        defaultZoom={14}
         options={{
           disableDefaultUI: true,
           zoomControl: true,
           styles: mapStyles,
         }}
-        defaultZoom={14}
-        onChildClick={(child) => console.log(child)}
+        margin={[50, 50, 50, 50]}
       >
-        {places.length &&
-          places.map((item, index) => {
-            if (item["name"]) {
-              return (
-                <div
-                  key={index}
-                  lat={item.latitude}
-                  lng={item.longitude}
-                  className="marker"
-                >
-                  <Paper elevation={3} className="paperContainer">
-                    <Typography variant="subtitle2" gutterBottom>
-                      {item.name}
-                    </Typography>
-                    <img
-                      src={
-                        item.photo
-                          ? item.photo.images.large.url
-                          : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
-                      }
-                      style={{ cursor: "pointer" }}
-                    />
-                    <Rating
-                      name="read-only"
-                      size="small"
-                      value={Number(item.rating)}
-                      readOnly
-                    />
-                  </Paper>
-                </div>
-              );
-            }
-          })}
+        {locations.map((location, index) => {
+          console.log(
+            index,
+            location.locationDetails.name,
+            location.locationDetails.latitude,
+            location.locationDetails.longitude
+          );
+          return (
+            <Marker
+              key={index}
+              lat={location.locationDetails?.latitude}
+              lng={location.locationDetails?.longitude}
+              place={location}
+              text={index + 1} // Marker text as the index number
+              color="blue" // Marker color; customize as needed
+            />
+          );
+        })}
       </GoogleMapReact>
     </div>
   );
